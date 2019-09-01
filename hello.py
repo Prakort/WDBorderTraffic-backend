360e08812b7a07af270a6e2167d3b0870e3290c1
lean prakort <prakort@leans-iMac.local>
2019-08-31 23:39:12 -0400

    hello

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+@app.route('/hello')
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
