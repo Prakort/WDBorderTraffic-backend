5ac7b94de10d7e45855a7bb2055a8a83e003c75f
lean prakort <prakort@leans-iMac.local>
2019-09-01 12:19:39 -0400

    jsonify

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 from bs4 import BeautifulSoup
 import requests
 app = Flask(__name__)
 
-@app.route('/')
+quarks = [{'name': 'up', 'charge': '+2/3'},
+          {'name': 'down', 'charge': '-1/3'},
+          {'name': 'charm', 'charge': '+2/3'},
+          {'name': 'strange', 'charge': '-1/3'}]
+
+@app.route('/',methods=['GET'])
 def hello():
     return 'Hello World!'
 
@@ -14,7 +21,7 @@ def index(request):
     return HttpResponse('<pre>' + r.text + '</pre>')
 
 @app.route('/data')
-def data():
+def data(request):
     other = "https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=3800&fbclid=IwAR0wQLJXEDPuLpnvYudjQ2OkrR_9OcxYBm_U1YvJCqm5SXjNH12dYGEm8Cc"
     res = requests.get(other)
     soup = BeautifulSoup(res.text,'html.parser')
@@ -23,3 +30,6 @@ def data():
     for string in table.strings:
         arr.append(repr(string))
     return arr
+@app.rout('/json',methods=['GET'])
+def json():
+    return jsonify({'quarks': quarks})
