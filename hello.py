a86716f95768aeaca37577755c425c1a768236e6
lean prakort <prakort@leans-iMac.local>
2019-09-01 12:32:07 -0400

    5th

--- hello.py
+++ hello.py
@@ -5,6 +5,17 @@ from bs4 import BeautifulSoup
 import requests
 app = Flask(__name__)
 
+def data():
+    other = "https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=3800&fbclid=IwAR0wQLJXEDPuLpnvYudjQ2OkrR_9OcxYBm_U1YvJCqm5SXjNH12dYGEm8Cc"
+    res = requests.get(other)
+    soup = BeautifulSoup(res.text,'html.parser')
+    table = soup.find('article')
+    arr = []
+    for string in table.strings:
+        arr.append(repr(string))
+    print(arr)
+    return jsonify({'arr': arr})
+
 quarks = [{'name': 'up', 'charge': '+2/3'},
           {'name': 'down', 'charge': '-1/3'},
           {'name': 'charm', 'charge': '+2/3'},
@@ -21,15 +32,9 @@ def index(request):
     return HttpResponse('<pre>' + r.text + '</pre>')
 
 @app.route('/data')
-def data(request):
-    other = "https://apps.cbp.gov/bwt/mobile.asp?action=n&pn=3800&fbclid=IwAR0wQLJXEDPuLpnvYudjQ2OkrR_9OcxYBm_U1YvJCqm5SXjNH12dYGEm8Cc"
-    res = requests.get(other)
-    soup = BeautifulSoup(res.text,'html.parser')
-    table = soup.find('article')
-    arr = []
-    for string in table.strings:
-        arr.append(repr(string))
-    return arr
+def dataone:
+    return data()
+
 @app.route('/json',methods=['GET'])
 def json():
     return jsonify({'quarks': quarks})
